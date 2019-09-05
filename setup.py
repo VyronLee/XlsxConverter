32862e0c5f7d1f922e3b191347ddc5664f2009a7
VyronLee <lwz_jz@hotmail.com>
2019-09-05 22:53:23 +0800

    Fix setup script.

--- setup.py
+++ setup.py
@@ -14,16 +14,16 @@
 
 from setuptools import setup, find_packages
 setup(
-    name = "xls2json",
+    name = "XlsxConverter",
     version = "1.0.0",
     packages = find_packages(),
 
-    install_requires = ['xlrd>=1.0.0'],
+    install_requires = ['xlrd>=1.0.0', 'protobuf>=3.9.1'],
 
     author = "VyronLee",
     author_email = "lwz_jz@hotmail.com",
-    description = "Convert xls or xlsx file to json.",
-    license = "Apache-2.0",
-    keywords = ["xls","xlsx","json"],
-    url = "https://github.com/VyronLee/xls2json"
+    description = "A set of tools for converting xls/xlsx file to multiple format, such as json, lua, protobuf, etc.",
+    license = "Apache 2.0",
+    keywords = ["xls","xlsx","json","lua","protobuf"],
+    url = "https://github.com/VyronLee/XlsxConverter"
 )
